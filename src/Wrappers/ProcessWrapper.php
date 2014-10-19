97bd4a29b170df6178f22fab00d86868cb0f9a38
larjohn <larjohn@gmail.com>
2014-10-19 16:59:38 +0300

    New functionality

--- src/Wrappers/ProcessWrapper.php
+++ src/Wrappers/ProcessWrapper.php
@@ -22,7 +22,7 @@ class ProcessWrapper extends Wrapper{
             return $process;
         }
         else{
-            $j_processes =  $this->_activiti->request("/repository/process-definitions", Activiti::GET);
+            $j_processes =  $this->_activiti->request("repository/process-definitions", Activiti::GET);
             $processes = array();
 
             foreach($j_processes->data as $j_process){

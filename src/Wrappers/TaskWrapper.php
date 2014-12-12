f30088db5a20a2422553b6388df59f96c7d5b7b0
larjohn <larjohn@gmail.com>
2014-12-13 00:44:23 +0200

    user only tasks

--- src/Wrappers/TaskWrapper.php
+++ src/Wrappers/TaskWrapper.php
@@ -25,7 +25,7 @@ class TaskWrapper extends Wrapper
             $task = new Task($j_task, $this);
             return $task;
         } else {
-            $j_tasks = $this->_activiti->request("runtime/tasks", Activiti::GET);
+            $j_tasks = $this->_activiti->get("runtime/tasks",array("candidateOrAssigned", $this->_activiti->username));
             $tasks = array();
 
             foreach ($j_tasks->data as $j_task) {

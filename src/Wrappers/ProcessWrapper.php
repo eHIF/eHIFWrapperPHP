c0a43bb3e49a38dfa3e6fac90c3d9434e8356c93
larjohn <larjohn@gmail.com>
2014-12-18 23:32:54 +0200

    caching update

--- src/Wrappers/ProcessWrapper.php
+++ src/Wrappers/ProcessWrapper.php
@@ -22,7 +22,7 @@ class ProcessWrapper extends Wrapper{
             return $process;
         }
         else{
-            $j_processes =  $this->_activiti->get("repository/process-definitions", array("startableByUser"=>$this->_activiti->username), true);
+            $j_processes =  $this->_activiti->get("repository/process-definitions", array("startableByUser"=>$this->_activiti->username, "size"=>100), false);
             $processes = array();
 
             foreach($j_processes->data as $j_process){
